eb25022ec730df54230e954422d26223b0dc8032
davidthus <dvdnicorici05@gmail.com>
2022-10-17 20:04:59 +0100

    delete boilerplate css files and add framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { Add, Edit, Feedback, Home, Roadmap } from "./pages";
 
 export interface user {
